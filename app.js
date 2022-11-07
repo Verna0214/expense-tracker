1ad11ab37143e9911c289198b1b39bfcaa647742
Verna0214 <yuhsichen0214@gmail.com>
2022-11-07 10:34:08 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const port = 3000
 
 // 僅在非正式環境時，使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -8,16 +8,21 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
+const port = 3000
+
+// MONGODB connect
 mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection
-
 db.on('error', () => {
   console.log('mongoose error!')
 })
-
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+// Set Handlebar
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.send('This is my expense-tracker app.')
