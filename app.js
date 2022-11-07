55e9b731b04b4856485aac6304e7224fb96c2ee9
Verna0214 <yuhsichen0214@gmail.com>
2022-11-07 10:20:07 +0800

    feat: add mongoose connection

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const port = 3000
-
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoose error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('This is my expense-tracker app.')
