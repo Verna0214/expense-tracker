0ae81970d0a403bf5a119b4e3baf67b33fb820a4
Verna0214 <yuhsichen0214@gmail.com>
2022-11-08 11:44:27 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Record = require('./models/record')
+const Category = require('./models/category')
+const User = require('./models/user')
 
 // 僅在非正式環境時，使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -24,8 +27,12 @@ db.once('open', () => {
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// index router
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
