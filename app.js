bee2d39bd85f8aebacd5259a5b2a8fc54aacc863
Verna0214 <yuhsichen0214@gmail.com>
2022-11-08 13:39:24 +0800

    modify index hbs

--- app.js
+++ app.js
@@ -24,14 +24,43 @@ db.once('open', () => {
 })
 
 // Set Handlebars
-app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs.engine({ 
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: {
+    categoryIcon(categoryId) {
+      switch (categoryId) {
+        case 1:
+          return 'fa-house'
+        case 2:
+          return 'fa-van-shuttle'
+        case 3:
+          return 'fa-face-grin-beam'
+        case 4:
+          return 'fa-utensils'
+        case 5:
+          return 'fa-pen'
+      }
+    },
+    dateFormat(date) {
+      return `${date.getFullYear()}-` + `${`0${date.getMonth() + 1}`.slice(-2)}-` + `${`0${date.getDate()}`.slice(-2)}`
+    }
+  }
+}))
 app.set('view engine', 'hbs')
 
 // index router
 app.get('/', (req, res) => {
   Record.find()
     .lean()
-    .then(records => res.render('index', { records }))
+    .sort({ date: 'desc' })
+    .then(records => {
+      let totalAmount = 0
+      for (let i = 0; i < records.length; i++) {
+        totalAmount += records[i].amount
+      }
+      res.render('index', { records, totalAmount })
+    })
     .catch(err => console.log(err))
 })
 
