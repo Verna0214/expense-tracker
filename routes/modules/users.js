5a0682b27ee1c22b77699cec536bb547be4f1fd5
Verna0214 <yuhsichen0214@gmail.com>
2022-11-10 10:02:28 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express()
+
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 
