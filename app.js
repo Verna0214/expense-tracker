7078ce3b16313d2f1bce51852c9110f8adabee86
Verna0214 <yuhsichen0214@gmail.com>
2022-11-12 11:24:10 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
@@ -56,10 +57,12 @@ app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -18,31 +18,47 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.findOne({ email })
-    .then(user => {
-      if (user) {
-        console.log('User already exists.')
-        res.render('register', {
-          name,
-          email,
-          password,
-          confirmPassword
-        })
-      } else {
-        return User.create({
-          name,
-          email,
-          password
-        })
-          .then(() => res.redirect('/'))
-          .catch(err => console.log(err))
-      }
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'All Forms Are Required！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'The Password and ConfirmPassword Do Not Match！ Please Check Your Password.' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
     })
-  .catch(err => console.log(err))
+  }
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('已註冊')
+      errors.push({ message: 'This Email Is Already Registered！' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return User.create({
+      name,
+      email,
+      password
+    })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', 'Successfully Logout！')
   res.redirect('/users/login')
 })
 
